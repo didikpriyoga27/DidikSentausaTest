49134b7c53f6574dae608e491eba26d65572c1b3
didikpriyoga27 <didikpriyoga27@gmail.com>
2024-11-23 17:48:59 +0700

    feat: account screen

--- src/account/screens/AccountScreen.tsx
+++ src/account/screens/AccountScreen.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, useWindowDimensions, View} from 'react-native';
 import BaseLayoutComponent from '../../shared/components/BaseLayoutComponent';
+import Header from '../../shared/components/Header';
+import Text from '../../shared/components/Text';
 
 const AccountScreen = () => {
+  const {width} = useWindowDimensions();
+
+  const account = {
+    name: 'Didik Priyoga',
+    email: 'didikpriyoga27@gmail.com',
+  };
+
   return (
     <BaseLayoutComponent>
-      <View className="p-4">
-        <Text className="text-white text-2xl">AccountScreen</Text>
+      <Header title="Akun Saya" />
+      <View className="p-4 gap-4">
+        <Text className="text-2xl font-semibold">Nama: {account.name}</Text>
+        <Text className="text-2xl">Email: {account.email}</Text>
+        <Text className="text-2xl">Kartu Membership:</Text>
+        <View>
+          <Image
+            source={require('../../shared/assets/images/card_background.jpg')}
+            className="rounded-md"
+            style={{width: width - 32, height: 240, resizeMode: 'cover'}}
+          />
+          <Text className="absolute top-4 right-8 text-2xl text-accent font-semibold">
+            {account.name}
+          </Text>
+          <Text className="absolute top-12 right-8">
+            Sejak 23 November 2024
+          </Text>
+        </View>
       </View>
     </BaseLayoutComponent>
   );
